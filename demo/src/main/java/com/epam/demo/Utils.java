721cea663ac3b79a85257f6146897adf0dedc1e0
sadyrin <sadyrin@gmail.com>
2023-02-18 22:55:52 +0200

    done task stage-2-module-1-build-tools-maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,8 +2,16 @@ package com.epam.demo;
 
 import java.util.List;
 
+import com.epam.utils.StringUtils;
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        Boolean isNegativeAbsent = true;
+
+        for(String s : args){
+            if(!StringUtils.isPositiveNumber(s)) isNegativeAbsent = false;
+        }
+
+        return isNegativeAbsent;
     }
 }
